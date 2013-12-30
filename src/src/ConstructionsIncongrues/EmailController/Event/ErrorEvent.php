777e6cc8d1acc9503be31d4ec719f34c22608798
Tristan Rivoallan <tristan@rivoallan.net>
2013-12-30 18:23:01 +0100

    [events] implemented ErrorEvent for broadcasting errors

---
+++ src/src/ConstructionsIncongrues/EmailController/Event/ErrorEvent.php
@@ -0,0 +1,34 @@
+<?php
+namespace ConstructionsIncongrues\EmailController\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+use ConstructionsIncongrues\EmailController\Message;
+
+class ErrorEvent extends Event
+{
+    private $error;
+    private $exception;
+    private $data = array();
+
+    public function __construct($error, \Exception $exception = null, $data = array())
+    {
+        $this->error = $error;
+        $this->exception = $exception;
+        $this->data = $data;
+    }
+
+    public function getError()
+    {
+        return $this->error;
+    }
+
+    public function getException()
+    {
+        return $this->exception;
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+}
